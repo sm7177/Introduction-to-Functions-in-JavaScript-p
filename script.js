c8bbe2e01c2150a3ca620711d4f14b665e380e10
sm7177 <subrinamaharjan2020@gmail.com>
2024-09-10 14:30:39 +0545

    project-calc

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=+prompt("Enter a")
+var b=prompt("Enter b")
+
+
+function multiply() {
+    var c = a*b
+    document.getElementById("m").innerHTML =c
+    }
+function division() {
+    var e = a/b
+    document.getElementById("d").innerHTML =e
+    }
